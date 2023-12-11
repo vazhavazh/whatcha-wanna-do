7d79b0154de9f8d616ee3c6c73547a7fc10ee007
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-12-11 21:20:38 +0200

    update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,28 @@
+import Link from "next/link";
+import localFont from "next/font/local";
 import { Medal } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+import { Button } from "@/components/ui/button";
+
+const headingFont = localFont({
+	src: "../../public/fonts/font.woff2",
+});
+
+const textFont = Poppins({
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 const MarketingPage = () => {
 	return (
 		<div className='flex items-center justify-center flex-col'>
-			<div className='flex items-center justify-center flex-col'>
+			<div
+				className={cn(
+					"flex items-center justify-center flex-col",
+					headingFont.className
+				)}>
 				<div
 					className='mb-4 flex items-center border shadow-sm p-4 
                 bg-amber-100 text-amber-700 rounded-full uppercase'>
@@ -14,16 +33,27 @@ const MarketingPage = () => {
 					Whatcha wanna do helps team move
 				</h1>
 				<div
-					className='text-3xl md:text-6xl bg-gradient-to-r from from-fuchsia-500 
-                to-pink-600 text-white px-4 p-2 rounded-md pb-4'>
+					className='text-3xl md:text-6xl bg-gradient-to-r from 
+                    from-fuchsia-500 to-pink-600 text-white px-4 p-2 rounded-md 
+                    pb-4'>
 					work forward.
 				</div>
 			</div>
-			<div>
+			<div
+				className={cn(
+					"text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+					textFont.className
+				)}>
 				Collaborate, manage projects, and reach new productivity peaks. From
 				hight rises to the home office the way your team works is unique -
 				accomplish it all with `Whatcha wanna do`.
 			</div>
+			<Button
+				className='mt-6'
+				size='lg'
+				asChild>
+				<Link href='/sign-up'>Get Whatcha wanna do for free</Link>
+			</Button>
 		</div>
 	);
 };
