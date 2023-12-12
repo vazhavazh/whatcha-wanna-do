01b53afe50f6c8743273230b1b0e7055189c581d
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-12-12 23:47:32 +0200

    Update layout.tsx

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -3,7 +3,7 @@ const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
 		<main className='pt-20 md:pt-24 px-4 max-w-6xl 2xl:max'>
 			<div className=' flex gap-x-7'>
 				<div className='w-64 shrink-0 hidden md:block'>
-					<Sidebar />
+					{/* <Sidebar /> */}
 				</div>
 				{children}
 			</div>
