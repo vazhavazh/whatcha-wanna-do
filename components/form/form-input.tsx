0a4e5d76d8433ea3d465e24e16aac2765471cc08
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-01-06 15:29:10 +0200

    create form input

---
+++ components/form/form-input.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { forwardRef } from "react";
+import { useFormStatus } from "react-dom";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+interface FormInputProps {
+	id: string;
+	label?: string;
+	type?: string;
+	placeholder?: string;
+	required?: boolean;
+	disabled?: boolean;
+	errors?: Record<string, string[] | undefined>;
+	className?: string;
+	defaultValue?: string;
+	onBlur?: () => void;
+}
+
+export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
+	(
+		{
+			id,
+			label,
+			type,
+			placeholder,
+			required,
+			disabled,
+			errors,
+			className,
+			defaultValue = "",
+			onBlur,
+		},
+		ref
+	) => {
+		const { pending } = useFormStatus();
+
+		return (
+			<div className='space y-2'>
+				<div className='space y-1'>
+					{label ? (
+						<Label
+							htmlFor={id}
+							className='text-xs font-semibold text-neutral-700'>
+							{" "}
+							Label{" "}
+						</Label>
+					) : null}
+					<Input
+						onBlur={onBlur}
+						id={id}
+						name={id}
+						defaultValue={defaultValue}
+						ref={ref}
+						required={required}
+						placeholder={placeholder}
+						type={type}
+						disabled={pending || disabled}
+					/>
+				</div>
+			</div>
+		);
+	}
+);
+
+FormInput.displayName = "FormInput";
--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback } from "react";
 
-import { ActionState } from "@/lib/create-safe-action";
+import { ActionState, FieldErrors } from "@/lib/create-safe-action";
 
 type Action<TInput, TOutput> = (
 	data: TInput
@@ -12,9 +12,52 @@ interface UseActionOptions<TOutput> {
 	onComplete?: () => void;
 }
 
-export const useAction = <TInput, TOutput> (
-    action: Action<TInput, TOutput>,
-    options: UseActionOptions<TOutput> = {}
+export const useAction = <TInput, TOutput>(
+	action: Action<TInput, TOutput>,
+	options: UseActionOptions<TOutput> = {}
 ) => {
-    const [fieldErro]
-}
+	const [fieldErrors, setFieldErrors] = useState<
+		FieldErrors<TInput> | undefined
+	>(undefined);
+
+	const [error, setError] = useState<string | undefined>(undefined);
+	const [data, setData] = useState<TOutput | undefined>(undefined);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const execute = useCallback(
+		async (input: TInput) => {
+			setIsLoading(true);
+			try {
+				const result = await action(input);
+				if (!result) {
+					return;
+				}
+				if (result.fieldErrors) {
+					setFieldErrors(result.fieldErrors);
+				}
+
+				if (result.error) {
+					setError(result.error);
+					options.onError?.(result.error);
+				}
+
+				if (result.data) {
+					setData(result.data);
+					options.onSuccess?.(result.data);
+				}
+			} finally {
+				setIsLoading(false);
+				options.onComplete?.();
+			}
+		},
+		[action, options]
+	);
+
+	return {
+		execute,
+		fieldErrors,
+		error,
+		data,
+		isLoading,
+	};
+};
