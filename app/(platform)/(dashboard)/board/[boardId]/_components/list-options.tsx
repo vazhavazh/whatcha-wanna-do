73a8e95b6807d1991be0aa8b7467c98411a48a98
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-01-24 22:32:40 +0200

    list options

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { ElementRef, useRef } from "react";
+
+import { toast } from "sonner";
+
 import { List } from "@prisma/client";
 import {
 	Popover,
@@ -7,6 +11,8 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/ui/popover";
+import { useAction } from "@/hooks/use-action";
+import { deleteList } from "@/actions/delete-list";
 import { Button } from "@/components/ui/button";
 import { MoreHorizontal, X } from "lucide-react";
 import { FormSubmit } from "@/components/form/form-submit";
@@ -18,6 +24,23 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+	const closeRef = useRef<ElementRef<"button">>(null);
+	const { execute: executeDelete } = useAction(deleteList, {
+		onSuccess: (data) => {
+			toast.success(`List "${data.title}" deleted`);
+			closeRef.current?.click();
+		},
+		onError: (error) => {
+			toast.error(error);
+		},
+	});
+
+	const onDelete = (formData: FormData) => {
+		const id = formData.get("id") as string;
+		const boardId = formData.get("boardId") as string;
+
+		executeDelete({ id, boardId });
+	};
 	return (
 		<Popover>
 			<PopoverTrigger asChild>
@@ -36,7 +59,9 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
                 pb-4'>
 					List actions
 				</div>
-				<PopoverClose asChild>
+				<PopoverClose
+					asChild
+					ref={closeRef}>
 					<Button
 						className='h-auto w-auto p-2 absolute top-2 right-2 
                     text-neutral-600'
@@ -72,7 +97,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
 					</FormSubmit>
 				</form>
 				<Separator />
-				<form>
+				<form action={onDelete}>
 					<input
 						hidden
 						name='id'
