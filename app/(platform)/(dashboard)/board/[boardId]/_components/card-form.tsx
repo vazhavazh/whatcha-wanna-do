ac4c8308c986703b3697ece983641b17b3e0c934
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-01-27 23:29:13 +0200

    create card

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useRef, ElementRef, KeyboardEventHandler, forwardRef } from "react";
+
+import { useParams } from "next/navigation";
+
+import { useOnClickOutside, useEventListener } from "usehooks-ts";
+
+import { FormSubmit } from "@/components/form/form-submit";
+import { FormTextarea } from "@/components/form/form-textarea";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-import { forwardRef } from "react";
+import { Plus, X } from "lucide-react";
+
+import { useAction } from "@/hooks/use-action";
+import { createCard } from "@/actions/create-card";
 
 interface CardFormProps {
 	listId: string;
@@ -13,6 +23,59 @@ interface CardFormProps {
 
 export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
 	({ listId, enableEditing, disableEditing, isEditing }, ref) => {
+		const params = useParams();
+		const formRef = useRef<ElementRef<"form">>(null);
+		const { execute, fieldErrors } = useAction(createCard);
+
+		const onKeyDown = (e: KeyboardEvent) => {
+			if (e.key === "Escape") {
+				disableEditing();
+			}
+		};
+
+		useOnClickOutside(formRef, disableEditing);
+		useEventListener("keydown", onKeyDown);
+
+		const onTextareaKeyDown: KeyboardEventHandler<HTMLAreaElement> = (e) => {
+			if (e.key === "Enter" && !e.shiftKey) {
+				e.preventDefault();
+				formRef.current?.requestSubmit();
+			}
+		};
+
+		const onSubmit = (formData: FormData) => {
+			const title = formData.get("title") as string;
+			const listId = formData.get("listId") as string;
+			const boardId = formData.get("boardId") as string;
+		};
+
+		if (isEditing) {
+			return (
+				<form className='m-1 py-0.5 px-1 space-y-4'>
+					<FormTextarea
+						id='title'
+						onKeyDown={() => {}}
+						ref={ref}
+						placeholder='Enter the title of this card...'
+					/>
+					<input
+						hidden
+						id='listId'
+						name='listId'
+						value={listId}
+					/>
+					<div className='flex items-center pag-x-1'>
+						<FormSubmit>Add card</FormSubmit>
+						<Button
+							onClick={disableEditing}
+							size='sm'
+							variant='ghost'>
+							<X className='h-5 w-5' />
+						</Button>
+					</div>
+				</form>
+			);
+		}
 		return (
 			<div className='pt-2 px-2'>
 				<Button
--- components/form/form-textarea.tsx
+++ components/form/form-textarea.tsx
@@ -2,6 +2,10 @@
 
 import { KeyboardEventHandler, forwardRef } from "react";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "../ui/textarea";
+import { cn } from "@/lib/utils";
+import { FormErrors } from "./form-errors";
+import { useFormStatus } from "react-dom";
 
 interface FormTextareaProps {
 	id: string;
@@ -12,7 +16,7 @@ interface FormTextareaProps {
 	errors?: Record<string, string[] | undefined>;
 	className?: string;
 	onBlur?: () => void;
-	onClick: () => void;
+	onClick?: () => void;
 	onKeyDown?: KeyboardEventHandler<HTMLTextAreaElement> | undefined;
 	defaultValue?: string;
 }
@@ -34,6 +38,7 @@ export const FormTextarea = forwardRef<HTMLTextAreaElement, FormTextareaProps>(
 		},
 		ref
 	) => {
+		const { pending } = useFormStatus();
 		return (
 			<div className='space-y-2 w-full'>
 				<div className='space-y-1 w-full'>
@@ -44,7 +49,29 @@ export const FormTextarea = forwardRef<HTMLTextAreaElement, FormTextareaProps>(
 							{label}
 						</Label>
 					) : null}
+					<Textarea
+						onKeyDown={onKeyDown}
+						onBlur={onBlur}
+						onClick={onClick}
+						ref={ref}
+						required={required}
+						placeholder={placeholder}
+						name={id}
+						id={id}
+						disabled={pending || disabled}
+						className={cn(
+							`resize-none focus-visible:ring-0 focus-visible:ring-offset-0 ring-0
+							focus:ring-0 outline-none shadow-sm`,
+							className
+						)}
+						aria-describedby={`${id}-error`}
+						defaultValue={defaultValue}
+					/>
 				</div>
+				<FormErrors
+					id={id}
+					errors={errors}
+				/>
 			</div>
 		);
 	}
