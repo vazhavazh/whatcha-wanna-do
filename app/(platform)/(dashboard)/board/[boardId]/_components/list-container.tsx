66766c1e0fa635b7e03b8c6a3b3d2c911820e915
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-01-29 23:52:44 +0200

    drag and drop server action

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -4,9 +4,13 @@ import { useEffect, useState } from "react";
 
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 
+import { useAction } from "@/hooks/use-action";
+import { updateListOrder } from "@/actions/update-list-order";
+
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
 import { ListItem } from "./list-item";
+import { toast } from "sonner";
 
 interface ListContainerProps {
 	data: ListWithCards[];
@@ -24,6 +28,15 @@ function reorder<T>(list: T[], startIndex: number, endIndex: number) {
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
 	const [orderedData, setOrderedData] = useState(data);
 
+	const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+		onSuccess: () => {
+			toast.success("List reordered");
+		},
+		onError: (error) => {
+			toast.error(error);
+		},
+	});
+
 	useEffect(() => {
 		setOrderedData(data);
 	}, [data]);
@@ -35,7 +48,7 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
 			return;
 		}
 
-		// ** if droped in the same position
+		// !! if dropped in the same position
 
 		if (
 			destination.droppableId === source.droppableId &&
@@ -44,10 +57,87 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
 			return;
 		}
 
-		// ** User moves a list
+		// !! User moves a list
 
 		if (type === "list") {
-			const items = reorder(orderedData, source.index, destination.index);
+			const items = reorder(orderedData, source.index, destination.index).map(
+				(item, index) => ({ ...item, order: index })
+			);
+
+			setOrderedData(items);
+			executeUpdateListOrder({ items, boardId });
+		}
+		if (type === "card") {
+			let newOrderedData = [...orderedData];
+
+			//!!  Source and destination list
+
+			const sourceList = newOrderedData.find(
+				(list) => list.id === source.droppableId
+			);
+
+			const destList = newOrderedData.find(
+				(list) => list.id === destination.droppableId
+			);
+
+			if (!sourceList || !destList) {
+				return;
+			}
+
+			// !! If cards exist on the sourceList
+
+			if (!sourceList.cards) {
+				sourceList.cards = [];
+			}
+
+			// !! Check if cards exist on the destList
+
+			if (!destList.cards) {
+				destination.cards = [];
+			}
+
+			// !! Moving the card in the same list
+
+			if (source.droppableId === destination.droppableId) {
+				const reorderedCards = reorder(
+					sourceList.cards,
+					source.index,
+					destination.index
+				);
+
+				reorderedCards.forEach((card, idx) => {
+					card.order = idx;
+				});
+
+				sourceList.cards = reorderedCards;
+
+				setOrderedData(newOrderedData);
+				// TODO: Trigger Server Action
+			}
+			// !! User moves card to another list
+			else {
+				// Remove card from source list
+				const [movedCard] = sourceList.cards.splice(source.index, 1);
+
+				// !! Assign the new listId to the card
+				movedCard.listId = destination.listId;
+
+				// !! Add card to the destination list
+				destList.cards.splice(destination.index, 0, movedCard);
+
+				sourceList.cards.forEach((card, idx) => {
+					card.order = idx;
+				});
+
+				// !! Update the order for each card in the destination list
+				destList.cards.forEach((card, idx) => {
+					card.order = idx;
+				});
+
+				setOrderedData(newOrderedData);
+
+				// TODO: Trigger Server action
+			}
 		}
 	};
 
