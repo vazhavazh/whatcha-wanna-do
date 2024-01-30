416d3473761452e3a708fedd327834ad571ab6ab
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-01-31 01:24:42 +0200

    Update description.tsx

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -10,6 +10,9 @@ import { AlignLeft } from "lucide-react";
 
 import { Skeleton } from "@/components/ui/skeleton";
 import { CardWithList } from "@/types";
+import { FormTextarea } from "@/components/form/form-textarea";
+import { FormSubmit } from "@/components/form/form-submit";
+import { Button } from "@/components/ui/button";
 
 interface DescriptionProps {
 	data: CardWithList;
@@ -40,20 +43,52 @@ export const Description = ({ data }: DescriptionProps) => {
 	};
 
 	useEventListener("keydown", onKeyDawn);
+	useOnClickOutside(formRef, disableEditing);
+
+	const onSubmit = (formData: FormData) => {
+		const description = formData.get("description") as string;
+		const boardId = params.boardId as string;
+
+		//  TODO: Execute
+	};
 
 	return (
 		<div className='flex items-start gap-x-3 w-full'>
 			<AlignLeft className='h-5 w-5 mt-0.5 text-neutral-700' />
 			<div className='w-full'>
 				<p className='font-semibold text-neutral-700 mb-2'>Description</p>
-				<div
-					role='button'
-					className='min-h-[78px] bg-neutral-200 text-sm font-medium py-3
+
+				{isEditing ? (
+					<form
+						ref={formRef}
+						className='space-y-2'>
+						<FormTextarea
+							id='description'
+							className='w-full mt-2'
+							placeholder='Add amore detailed description'
+							defaultValue={data.description || undefined}
+						/>
+						<div className='flex items-center gap-x-2'>
+							<FormSubmit>Save</FormSubmit>
+							<Button
+								type='button'
+								onClick={disableEditing}
+								variant='ghost'
+								size='sm'>
+								Cancel
+							</Button>
+						</div>
+					</form>
+				) : (
+					<div
+						onClick={enableEditing}
+						role='button'
+						className='min-h-[78px] bg-neutral-200 text-sm font-medium py-3
                     px-3.5 rounded-md'>
-					{data.description || "Add a more detailed description"}
-				</div>
+						{data.description || "Add a more detailed description"}
+					</div>
+				)}
 			</div>
-			{data.description}
 		</div>
 	);
 };
